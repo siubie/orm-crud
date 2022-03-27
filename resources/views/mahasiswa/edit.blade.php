2111163e2483050ad1ae6dd1444ae008c0f54d58
burntato <kineticz300@gmail.com>
2022-03-27 14:45:52 +0700

    update views

---
+++ resources/views/mahasiswa/edit.blade.php
@@ -0,0 +1,51 @@
+@extends('mahasiswa.create')
+
+@section('content')
+
+    <div class="container mt-5">
+        <div class="row justify-content-center align-items-center">
+            <div class="card" style="width: 24rem">
+                <div class="card-header">
+                    Edit Mahasiswa
+                </div>
+                <div class="card-body">
+                    @if ($errors->any())
+                        <div class="alert alert-danger">
+                            <strong>Whoops!</strong>There were some problems with your input. <br><br>
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
+                    <form method="POST" action="{{ route('mahasiswa.update', $mahasiswa->id) }}" id="myForm">
+                        @csrf
+                        @method('PUT')
+                        <div class="form-group">
+                            <label for="nim">Nim</label>
+                            <input type="text" class="form-control" id="nim" name="nim" aria-describedby="Nim"
+                                value="{{ $mahasiswa->nim }}">
+                        </div>
+                        <div class="form-group">
+                            <label for="nama">Nama</label>
+                            <input type="text" class="form-control" id="nama" name="nama" aria-describedby="Nama"
+                                value="{{ $mahasiswa->nama }}">
+                        </div>
+                        <div class="form-group">
+                            <label for="kelas">Kelas</label>
+                            <input type="text" class="form-control" id="kelas" name="kelas" aria-describedby="Kelas"
+                                value="{{ $mahasiswa->kelas }}">
+                        </div>
+                        <div class="form-group">
+                            <label for="jurusan">Jurusan</label>
+                            <input type="text" class="form-control" id="jurusan" name="jurusan" aria-describedby="Jurusan"
+                                value="{{ $mahasiswa->jurusan }}">
+                        </div>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
