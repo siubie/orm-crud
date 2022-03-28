db8c78bf34ac687493cc87c2d971f530f3f9ef2d
Maulana Ghofurur Rohim <lanabackend@gmail.com>
2022-03-28 22:15:32 +0700

    update action

--- resources/views/mahasiswa/index.blade.php
+++ resources/views/mahasiswa/index.blade.php
@@ -2,6 +2,14 @@
 
 @section('content')
 <a href="{{ route('mahasiswa.create') }}" class="btn btn-primary mb-2">Input Mahasiswa</a>
+
+<!-- alert -->
+<div class="row mb-2">
+  @if(session('success'))
+    <div class="alert alert-success">{{ session('success') }}</div>
+  @endif
+</div>
+
 <table class="table table-striped table-hover">
   <thead class="text-capitalize">
     <th>no</th>
@@ -9,6 +17,7 @@
     <th>nama</th>
     <th>kelas</th>
     <th>jurusan</th>
+    <th>action</th>
   </thead>
   <tbody>
     @foreach($mahasiswa as $mhs)
@@ -18,8 +27,27 @@
         <td>{{ $mhs->nama ?? '' }}</td>
         <td>{{ $mhs->kelas ?? '' }}</td>
         <td>{{ $mhs->jurusan ?? '' }}</td>
+        <td>
+          <a href="{{ route('mahasiswa.show', $mhs->id_mahasiswa) }}" class="badge bg-primary text-decoration-none">show</a>
+          <a href="{{ route('mahasiswa.edit', $mhs->id_mahasiswa) }}" class="badge bg-warning text-decoration-none">edit</a>
+          <a href="#"
+            class="badge bg-danger text-decoration-none"
+            onclick="event.preventDefault(); document.getElementById('form-destroy-{{ $mhs->id_mahasiswa }}').submit()">delete</a>
+          <form action="{{ route('mahasiswa.destroy', $mhs->id_mahasiswa) }}" method="post" id="form-destroy-{{ $mhs->id_mahasiswa }}">
+            @csrf
+            @method('DELETE')
+          </form>
+        </td>
       </tr>
     @endforeach
   </tbody>
+  <tfoot>
+    <th>no</th>
+    <th>nim</th>
+    <th>nama</th>
+    <th>kelas</th>
+    <th>jurusan</th>
+    <th>Action</th>
+  </tfoot>
 </table>
 @endsection
