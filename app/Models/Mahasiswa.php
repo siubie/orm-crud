d2b0bd25e9937b979f7f9095e94c30a65922cf94
vinkharisma <vinkharismadzania@gmail.com>
2022-03-29 19:36:34 +0700

    Praktikum CRUD dengan ORM

--- app/Models/Mahasiswa.php
+++ app/Models/Mahasiswa.php
@@ -5,11 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Mahasiswa extends Model
+class Mahasiswa extends Model //Definisi Model
 {
     use HasFactory;
-    protected $table = "Mahasiswa";
-    protected $primaryKey = "id_mahasiswa";
+    protected $table = "Mahasiswa"; // Eloquent akan membuat model mahasiswa menyimpan record di tabel mahasiswa
+    protected $primaryKey = "id_mahasiswa"; // Memanggil isi DB Dengan primarykey
 
     protected $fillable = [
         'nim',
