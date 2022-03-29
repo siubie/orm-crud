1637be249a32f5c752a19e8dd0f038864a1c901b
aldanelva <aldamondel@gmail.com>
2022-03-29 14:20:59 +0700

    web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\MahasiswaController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,11 +16,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::resource('/mahasiswa', MahasiswaController::class);
+Route::get('/', [MahasiswaController::class, 'index']);
 
 Auth::routes();
 
